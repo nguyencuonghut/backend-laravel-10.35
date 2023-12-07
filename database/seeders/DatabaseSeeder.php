3f3193d851c3eb7856a4e1fda8c5fbca4ae71b18
Tony Ng <nguyencuonghut55@gmal.com>
2023-12-07 14:00:16 +0700

    Add is_disabled to User table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Tony',
+            'email' => 'nguyenvancuong@honghafeed.com.vn',
+            'password' => bcrypt('Hongha@123'),
+            'is_disabled' => false,
+        ]);
     }
 }
