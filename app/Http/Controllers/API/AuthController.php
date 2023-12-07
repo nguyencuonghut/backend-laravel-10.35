d1c51f0067aee62efc9f1be0a73d1796010a01b8
Tony Ng <nguyencuonghut55@gmal.com>
2023-12-07 14:42:46 +0700

    Add login function

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $rules = array(
+            'email'    => 'required|email|exists:users', // make sure the email is an actual email
+            'password' => 'required' // password can only be alphanumeric and has to be greater than 3 characters
+        );
+
+        $messages = [
+            'email.required' => 'Bạn phải nhập địa chỉ email.',
+            'email.email' => 'Email sai định dạng.',
+            'email.exists' => 'Email không tồn tại trên hệ thống.',
+            'password.required' => 'Bạn phải nhập mật khẩu.'
+        ];
+        $request->validate($rules,$messages);
+
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+            ], true)) {
+            //Prevent disable User from logging
+            if (Auth::user()->is_disabled) {
+                Auth::logout();
+                return response([
+                    'error' => 'Tài khoản đã bị khóa!'
+                ], 422);
+            } else {
+                $user = Auth::user();
+                $token = $user->createToken('main')->plainTextToken;
+                return response([
+                    'user' => $user,
+                    'token' => $token
+                ]);
+            }
+        } else {
+            return response([
+                'error' => 'Email hoặc mật khẩu không đúng!'
+            ], 422);
+        }
+    }
+}
